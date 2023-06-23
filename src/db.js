7809a2ac238f297184b96f60d91510036409761a
kongchae <qwa30428@naver.com>
2023-06-23 16:37:30 +0100

    Memory table 변경

--- src/db.js
+++ src/db.js
@@ -6,21 +6,9 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function insertMemory({ content }) {
+export async function insertMemory({ content, category }) {
   const imageUrl = "";
-  const tags = [];
-  const sentiments = {
-    calliope: 1,
-    clio: 0,
-    erato: 0,
-    euterpe: 0,
-    melpomene: 0,
-    polyhymnia: 0,
-    terpsichore: 0,
-    thalia: 0,
-    urania: 0,
-  };
-  const memory = { content, imageUrl, tags, ...sentiments };
+  const memory = { content, imageUrl, ...category };
   const { data, error } = await supabase
     .from("memories")
     .insert(memory)
--- src/router.js
+++ src/router.js
@@ -8,14 +8,14 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/memory", async (req, res) => {
-  const { content } = req.body;
+  const { content, category } = req.body;
 
   if (!content) {
     res.status(400).send("Missing content in body");
   }
 
   try {
-    const memory = await DB.insertMemory({ content });
+    const memory = await DB.insertMemory({ content, category });
     res.status(200).send(memory);
   } catch {
     res.status(500).send("Something went wrong in DB");
