f6e430a03592efccf159e1707bd2d46877c2205d
kongchae <qwa30428@naver.com>
2023-07-14 13:41:21 +0100

    read Memories

--- src/router.js
+++ src/router.js
@@ -8,6 +8,28 @@ router.get("/", async (req, res) => {
   res.send("Server is running ⚡️");
 });
 
+router.get("/memory", async (req, res) => {
+  const { id, category } = req.query;
+
+  if (id !== undefined) { // get one memory
+    try {
+      const memory = await DB.get({ id });
+      return res.status(200).send(memory);
+    } catch (dbError) {
+      console.error('Error executing SELECT query:', dbError.message);
+      return res.status(500).send("Something went wrong in DB");
+    }
+  } else {
+    try {
+      const memories = await DB.getAll({ category });
+      return res.status(200).send(memories);
+    } catch (dbError) {
+      console.error('Error executing SELECT query:', dbError.message);
+      return res.status(500).send("Something went wrong in DB");
+    }
+  }
+})
+
 router.post("/memory", async (req, res) => {
   const { content, category, sentiments } = req.body;
 
